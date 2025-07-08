dd8b3555f5063de25c2df0133a4687041b25975e
pthibaud <pthibaud@users.noreply.github.com>
2025-07-08 21:59:34 +0200

    abstract focus

--- test/arxiv.py
+++ test/arxiv.py
@@ -4,15 +4,14 @@ from arxiv_client import Query,Category
 client = ArXivClient()        
         # Search papers
 
-query=Query(abstract_keywords=["spintronic"], keywords=["spintronic"], categories=[Category.COND_MAT_MES_HALL] , max_results = 25)
-
-print(query)
+query=Query(abstract_keywords=["spintronic"], keywords=["spintronic"], categories=[Category.COND_MAT_MES_HALL] , max_results = 100)
 
 try :
     papers = client.search(query = query)
 
 finally:
     results = []
+    abstracts = []
     for i, paper in enumerate(papers):
         paper_info = f"### {i}. {"".join(paper.title.split("\n "))}\n\n"
         paper_info += f"**Authors:** {", ".join(paper.authors)}\n\n"
@@ -20,4 +19,33 @@ finally:
         paper_info += f"**Link:** [{paper.pdf_url}]({paper.pdf_url})\n\n"
         paper_info += f"**Abstract:** {" ".join(paper.summary.split("\n"))}\n"
         results.append(paper_info)
+        abstracts.append(paper.summary)
+        
         print(paper_info)
+
+from config import MISTRAL_CONFIG
+from mistralai import Mistral
+
+api_key = MISTRAL_CONFIG.get("api_key")
+model = "mistral-large-latest"
+
+client = Mistral(api_key=api_key)
+
+chat_response = client.chat.complete(
+    model = model,
+    messages = [
+        {
+            "role": "system",
+            "content": """
+            You are an AI librarian classifying research articles.
+
+            Here are {len(abstracts)} abstracts:
+            {abstracts}
+
+            Group them by topic and suggest folder names for each group.
+            """
+        }
+    ]
+)
+
+print(chat_response.choices[0].message.content)
